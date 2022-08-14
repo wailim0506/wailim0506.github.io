90762b5fbf29e758ba9c837188e5436d94ac3e12
wai <wailim0506@icloud.com>
2022-08-14 20:35:00 +0100

    Create script.js

---
+++ Intro-HK/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    var screenWidth = $(window).width()
+	console.log("Screen Width: "+screenWidth +"px")
+	$("body").fadeIn(500)
+	$("#disneyland").css("display","none")
+	$("#lwf").css("display","none")
+	$("#templeStreet").css("display","none")
+    $("#footer").css("display","none")
+    $(".one").css("display","none")
+    $(".two").css("display","none")
+    $(".three").css("display","none")
+    
+	$(window).scroll(function(){
+		var x = $(window).scrollTop()
+        if (screenWidth>=1200){
+            if (x>=406){
+			    $("#disneyland").fadeIn(1000)
+                $(".one").fadeIn(1000)
+                $("#footer").fadeIn(1000)
+		    }
+		    if (x>=928){
+			    $("#lwf").fadeIn(1000)
+                $(".two").fadeIn(1000)
+		    }
+		    if (x>=1305){
+			    $("#templeStreet").fadeIn(1000)
+                $(".three").fadeIn(1000)
+		    }
+		    
+        }
+
+        else if (screenWidth>=992){
+            if (x>=486){
+			    $("#disneyland").fadeIn(1000)
+                $(".one").fadeIn(1000)
+                $("#footer").fadeIn(1000)
+		    }
+		    if (x>=988){
+			    $("#lwf").fadeIn(1000)
+                $(".two").fadeIn(1000)
+		    }
+		    if (x>=1405){
+			    $("#templeStreet").fadeIn(1000)
+                $(".three").fadeIn(1000)
+		    }
+		    
+        }
+        else{
+            if (x>=990){
+			    $("#disneyland").fadeIn(1000)
+                $(".one").fadeIn(1000)
+                $("#footer").fadeIn(1000)
+		    }
+		    if (x>=1625){
+			    $("#lwf").fadeIn(1000)
+                $(".two").fadeIn(1000)
+		    }
+		    if (x>=2365){
+			    $("#templeStreet").fadeIn(1000)
+                $(".three").fadeIn(1000)
+		    }
+		    
+        }
+		
+		
+		
+		
+		
+		//console.log(x)
+		
+	})
+})
+
+//use scroll() to excute function when user scroll the page
+//use scrollTop() to check the position of the page in px
+//disneyland 506px
+//lwf 988px
