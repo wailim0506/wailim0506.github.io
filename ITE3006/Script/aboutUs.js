b8929ca5bdd6a6615728691da91f2e859a6993e7
wai <wailim0506@icloud.com>
2024-03-29 01:30:12 +0800

    d

--- ITE3006/Script/aboutUs.js
+++ ITE3006/Script/aboutUs.js
@@ -7,3 +7,7 @@ function hide(){   //hide the hidden menu
     document.getElementById('hidden_menu').style.display = 'none';
     document.getElementById('body').style.overflow = 'auto'; //allow scrolling so the index page can be scrolled after the hidden menu is hide
 }
+
+function thank(){
+    window.alert("Thank you for your comment!");
+}
