0f0b033bd657a187b3e9be287900aa1be17366f9
wai <wailim0506@icloud.com>
2024-03-31 21:15:32 +0800

    e

--- ITE3006/Script/player/player.js
+++ ITE3006/Script/player/player.js
@@ -7,3 +7,8 @@ function hide(){   //hide the hidden menu
     document.getElementById('hidden_menu').style.display = 'none';
     document.getElementById('body').style.overflow = 'auto'; //allow scrolling so the index page can be scrolled after the hidden menu is hide
 }
+
+function showBio(){  //show the hidden bio
+    document.getElementById('hidden_bio').style.display = 'block'; //show the hidden bio
+    document.getElementById('readmore_button').style.display = 'none'; //hide the read more button
+}
